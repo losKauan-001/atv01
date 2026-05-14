7b31daf4c0a178f268f0b07e9d07dc9d122e3152
Kauã Argolo Guilherme Oliveira <marcos_vargas@UCB-USP-NCL6-08.adventistas.local>
2026-05-14 09:58:46 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+let h1 = document.querySelector("h1")
+let botao = document.querySelector("#save")
+
+function alterarH1(){
+    let nome = document.querySelector("#novo-item").value;
+    
+    if (nome === ""){
+        h1.innerText = "Compras da semana"
+    } else {
+        h1.innerText = nome
+    }
+    
+}
+
+
+
+
+
+
+
+
+    NOTA:
+        querySelector: procura por um "H1" por exemplo.
+        Ele acha o primeiro h1 em ordem execução.
+
+        getElementById: procura pelo id.
+
+        addEventListener: adiciona um evento ao botão.
+*/
